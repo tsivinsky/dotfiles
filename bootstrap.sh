6786a4ea3762f88b5627b1315ce0471fb6baea9a
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-09-09 13:41:43 +0300

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,13 @@
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Source .vimrc
+source ~/.vimrc
+
+# Create symlinks
+ln -s $PWD/i3_config $HOME/.config/i3/config
+ln -s $PWD/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+ln -s $PWD/polybar/config $HOME/.config/polybar/config
+ln -s $PWD/polybar/launch.sh $HOME/.config/polybar/launch.sh
+ln -s $PWD/rofi_config.rasi $HOME/.config/rofi/config.rasi
