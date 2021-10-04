4382327eb07d94a7aa7dc52fc6241b61fe9b8930
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-10-04 20:36:46 +0300

    Add .gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,7 @@ i3Dir="$HOME/.config/i3"
 polybarDir="$HOME/.config/polybar"
 rofiDir="$HOME/.config/rofi"
 picomFile="$HOME/.config/picom.conf"
+gitconfigFile="$HOME/.gitconfig"
 
 # Install all configs
 if [ "$1" = "-S" ] ; then
@@ -31,6 +32,7 @@ ln -s $PWD/alacritty $alacrittyDir
 ln -s $PWD/polybar $polybarDir
 ln -s $PWD/rofi $rofiDir
 ln -s $PWD/picom.conf $picomFile
+ln -s $PWD/.gitconfig $gitconfigFile
 
 # Install vim-plug
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -42,7 +44,7 @@ fi
 # Remove all configs
 if [ "$1" = "-R" ] ; then
 
-cd && rm -rf .bashrc $nvimDir $alacrittyDir $i3Dir $polybarDir $rofiDir $picomFile && cd -
+cd && rm -rf .bashrc $nvimDir $alacrittyDir $i3Dir $polybarDir $rofiDir $picomFile $gitconfigFile && cd -
 
 fi
 
