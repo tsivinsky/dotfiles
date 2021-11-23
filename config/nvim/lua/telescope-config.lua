bf9195389b65cb49f7a324081844e214069b6955
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-11-23 19:41:00 +0300

    Nuked neovim config to start fresh

--- config/nvim/lua/telescope-config.lua
+++
@@ -1,2 +0,0 @@
-
-require("telescope").setup({ defaults = { sorting_strategy = "ascending", file_ignore_patterns = { ".git", "node_modules" } } })
--- config/nvim/lua/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require("nvim-treesitter.configs").setup({
-  highlight = { enable = true },
-  rainbow = { enable = true, extended_mode = true, disable = { "html" } },
-  autotag = { enable = true },
-  context_commentstring = { enable = true }
-})
