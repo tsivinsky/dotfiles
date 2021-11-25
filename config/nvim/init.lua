4f6e748e71de7767d9d7b118f6b32df54b05b048
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-11-25 14:52:47 +0300

    Add open_on_tab and auto_close options to nvim-tree config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -104,6 +104,8 @@ require("nvim-treesitter.configs").setup({
 require("nvim-tree").setup({
   open_on_setup = true,
   hijack_cursor = true,
+  auto_close = true,
+  open_on_tab = true,
   update_cwd = true,
   update_focused_file = { enable = true },
   view = { width = 30, side = "left", auto_resize = true },
