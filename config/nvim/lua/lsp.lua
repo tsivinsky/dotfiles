49ae1d67e2b0b3383bde1bdec4d5251934ad0147
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-12-11 16:21:50 +0300

    Remove javascript and typescript from emmet lsp

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -129,9 +129,8 @@ configs.ls_emmet = {
     default_config = {
         cmd = {'ls_emmet', '--stdio'},
         filetypes = {
-            'html', 'css', 'scss', 'javascript', 'javascriptreact',
-            'typescript', 'typescriptreact', 'haml', 'xml', 'xsl', 'pug',
-            'slim', 'sass', 'stylus', 'less', 'sss'
+            'html', 'css', 'scss', 'javascriptreact', 'typescriptreact', 'haml',
+            'xml', 'xsl', 'pug', 'slim', 'sass', 'stylus', 'less', 'sss'
         },
         root_dir = function(fname) return vim.loop.cwd() end,
         settings = {}
