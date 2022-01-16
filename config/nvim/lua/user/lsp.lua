b2931e8835f9fe290eb753c4f45987b487770107
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-01-16 10:26:56 +0300

    Remove diagnostics from signcolumn

--- config/nvim/lua/user/lsp.lua
+++ config/nvim/lua/user/lsp.lua
@@ -174,6 +174,7 @@ end)
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 	update_in_insert = true,
 	virtual_text = false,
+	signs = false,
 })
 
 require("luasnip.loaders.from_vscode").load()
