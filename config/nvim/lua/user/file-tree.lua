5e0ddabd43784f88d6f98102773f63c7b8e12f0a
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-01-29 14:41:45 +0300

    Set group_empty option in nvim-tree

--- config/nvim/lua/user/file-tree.lua
+++ config/nvim/lua/user/file-tree.lua
@@ -20,6 +20,7 @@ vim.g.nvim_tree_icons = {
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_group_empty = 1
 
 require("nvim-tree").setup({
   disable_netrw = true,
