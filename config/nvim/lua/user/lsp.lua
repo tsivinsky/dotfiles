df31f8bfe01a7cf944f8fa3b88b5cb397ccdd437
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-02-04 18:34:35 +0300

    show errors and warnings in virtual text

--- config/nvim/lua/user/lsp.lua
+++ config/nvim/lua/user/lsp.lua
@@ -178,7 +178,7 @@ end)
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   update_in_insert = true,
   virtual_text = {
-    severity_limit = "Error",
+    severity_limit = "Warning",
   },
   signs = false,
 })
