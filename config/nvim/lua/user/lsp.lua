cc7eeb9212e65cc436a7113aed346cbf80e86c80
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-02-04 23:53:29 +0300

    enable snippet support for emmet and disable html formatting

--- config/nvim/lua/user/lsp.lua
+++ config/nvim/lua/user/lsp.lua
@@ -130,8 +130,9 @@ cmp.setup.cmdline(":", {
 })
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local lsps_with_disabled_formatting = { "tsserver", "gopls", "jsonls" }
+local lsps_with_disabled_formatting = { "tsserver", "gopls", "jsonls", "html" }
 
 local on_attach = function(client, bufnr)
   if list_includes_item(lsps_with_disabled_formatting, client.name) then
