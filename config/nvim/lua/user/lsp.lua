b345eaf63cd006f63675641668bcb6fcb07a57a8
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-02-10 18:53:32 +0300

    remove severity_limit lsp setting

--- config/nvim/lua/user/lsp.lua
+++ config/nvim/lua/user/lsp.lua
@@ -178,8 +178,5 @@ end)
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   update_in_insert = false,
-  virtual_text = {
-    severity_limit = "Warning",
-  },
   signs = false,
 })
