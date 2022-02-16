649f5b6b79b145fb41a6d741584bb51a693c523c
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-02-16 11:11:59 +0300

    remove telescope-fzf-native

--- config/nvim/lua/user/telescope.lua
+++ config/nvim/lua/user/telescope.lua
@@ -5,14 +5,4 @@ telescope.setup({
     sorting_strategy = "ascending",
     file_ignore_patterns = { ".git/", "node_modules/" },
   },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    },
-  },
 })
-
-telescope.load_extension("fzf")
