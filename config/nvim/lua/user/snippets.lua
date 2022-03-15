bc060ef0119a7f2566064ff31611a82112aafd92
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-03-15 22:32:19 +0300

    add `clg` snippet

--- config/nvim/lua/user/snippets.lua
+++ config/nvim/lua/user/snippets.lua
@@ -1,1 +1,27 @@
+local ls = require("luasnip")
+local s, i = ls.s, ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+local clg_snippet = s(
+  "clg",
+  fmt([[console.log({});]], {
+    i(0),
+  })
+)
+
+ls.snippets = {
+  javascript = {
+    clg_snippet,
+  },
+  typescript = {
+    clg_snippet,
+  },
+  javascriptreact = {
+    clg_snippet,
+  },
+  typescriptreact = {
+    clg_snippet,
+  },
+}
+
 require("luasnip.loaders.from_vscode").load()
