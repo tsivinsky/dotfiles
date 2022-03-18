728ba93db5396867861ed7a35764ca3044e39a3d
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-03-18 20:37:44 +0300

    change theme to edge

--- config/nvim/lua/user/theme.lua
+++ config/nvim/lua/user/theme.lua
@@ -1,6 +1,7 @@
-vim.g.tokyonight_style = "night"
+vim.g.edge_diagnostic_virtual_text = "colored"
+vim.g.edge_better_performance = 1
 
 vim.cmd([[
-colorscheme tokyonight
+colorscheme edge
 hi SpellBad guifg=red
 ]])
