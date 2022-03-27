43ad653bb325686e06a9f1a9a7f4097b9c8b5981
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-03-27 15:06:53 +0300

    back to tokyonight

--- neovim/.config/nvim/lua/user/theme.lua
+++ neovim/.config/nvim/lua/user/theme.lua
@@ -1,7 +1,8 @@
 vim.g.edge_diagnostic_virtual_text = "colored"
 vim.g.edge_better_performance = 1
+vim.g.tokyonight_style = "night"
 
 vim.cmd([[
-colorscheme edge
+colorscheme tokyonight
 hi SpellBad guifg=red
 ]])
