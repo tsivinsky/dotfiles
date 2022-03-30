22bf50912701a1e7b3262572430f65ed0396cf71
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-03-30 22:58:22 +0300

    move nvim-cmp setup to comp.lua

---
+++ neovim/.config/nvim/lua/user/comp.lua
@@ -0,0 +1,109 @@
+local cmp = require("cmp")
+
+local kind_icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
+local completion_trigger = "<C-space>"
+if vim.fn.has("win32") == 1 then
+  completion_trigger = "<C-y>"
+end
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    [completion_trigger] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.close()
+      else
+        cmp.complete()
+      end
+    end),
+    ["<TAB>"] = cmp.mapping.confirm({
+      select = true,
+      behavior = cmp.SelectBehavior.Insert,
+    }),
+    ["<A-j>"] = cmp.mapping.scroll_docs(4),
+    ["<A-k>"] = cmp.mapping.scroll_docs(-4),
+  },
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "path" },
+    { name = "buffer" },
+  }),
+  completion = {
+    completeopt = "menu,menuone,noselect,noinsert,preview",
+  },
+  experimental = {
+    ghost_text = true,
+  },
+  sorting = {
+    comparators = {
+      cmp.config.compare.score,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.kind,
+    },
+  },
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    zindex = 999,
+    format = { "markdown" },
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        luasnip = "[Snippet]",
+        nvim_lua = "[Lua]",
+        path = "[File]",
+      })[entry.source.name]
+
+      return vim_item
+    end,
+  },
+})
+
+cmp.setup.cmdline("/", {
+  sources = {
+    { name = "cmdline" },
+  },
+})
+
+cmp.setup.cmdline(":", {
+  sources = {
+    { name = "cmdline" },
+  },
+})
