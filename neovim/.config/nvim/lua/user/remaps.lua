81bc1b9bdbb8bb336c5d78b7a6e603f958bfe935
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-04-09 22:24:03 +0300

    open horizontal terminal at the very bottom

--- neovim/.config/nvim/lua/user/remaps.lua
+++ neovim/.config/nvim/lua/user/remaps.lua
@@ -36,7 +36,7 @@ vim.keymap.set("v", ">", ">gv")
 
 -- Terminal
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
-vim.keymap.set("n", "Th", ":split | :term<CR>")
+vim.keymap.set("n", "Th", ":split | :term<CR> <C-W>J")
 vim.keymap.set("n", "Tv", ":vs | :term<CR>")
 
 -- Tabs
@@ -112,6 +112,7 @@ vim.keymap.set("n", "<leader>gg", ":G<CR>")
 vim.keymap.set("n", "<leader>go", function()
   git.open_commit_on_github()
 end)
+vim.keymap.set("n", "<leader>gh", ":Octo actions<CR>")
 
 -- Refactoring
 vim.keymap.set("n", "<leader>ri", function()
