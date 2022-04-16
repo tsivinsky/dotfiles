7c58a63b8e314dfe2819c5385377e13f21f22b3f
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-04-16 12:29:45 +0300

    add `GitShowCommit` command

--- neovim/.config/nvim/lua/user/cmds.lua
+++ neovim/.config/nvim/lua/user/cmds.lua
@@ -1,3 +1,11 @@
+local git = require("user.git")
+
 vim.api.nvim_add_user_command("OrganizeImports", function()
   require("user.utils").lsp_organize_imports()
+end, {})
+
+vim.api.nvim_add_user_command("GitShowCommit", function()
+  local commit = git.get_commit_hash_for_current_line()
+
+  vim.cmd(":Git show " .. commit)
 end, {})
