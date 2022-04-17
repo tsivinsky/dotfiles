3878d429704e4728be66f339c73d416c0d2eedbf
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-04-17 13:39:07 +0300

    change lua command api

--- neovim/.config/nvim/lua/user/cmds.lua
+++ neovim/.config/nvim/lua/user/cmds.lua
@@ -1,10 +1,10 @@
 local git = require("user.git")
 
-vim.api.nvim_add_user_command("OrganizeImports", function()
+vim.api.nvim_create_user_command("OrganizeImports", function()
   require("user.utils").lsp_organize_imports()
 end, {})
 
-vim.api.nvim_add_user_command("GitShowCommit", function()
+vim.api.nvim_create_user_command("GitShowCommit", function()
   local commit = git.get_commit_hash_for_current_line()
 
   vim.cmd(":Git show " .. commit)
