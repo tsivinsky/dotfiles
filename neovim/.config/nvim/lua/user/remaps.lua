62083778f83662ef0583ad62e657da513fbb4bfe
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-04-23 11:58:25 +0300

    remove resize remaps

--- neovim/.config/nvim/lua/user/remaps.lua
+++ neovim/.config/nvim/lua/user/remaps.lua
@@ -25,12 +25,6 @@ vim.keymap.set("n", "<leader>l", "<C-w>l")
 vim.keymap.set("n", "<leader>j", "<C-w>j")
 vim.keymap.set("n", "<leader>k", "<C-w>k")
 
--- Resize splits
-vim.keymap.set("n", "<C-j>", ":resize -2<CR>")
-vim.keymap.set("n", "<C-k>", ":resize +2<CR>")
-vim.keymap.set("n", "<C-h>", ":vert resize -5<CR>")
-vim.keymap.set("n", "<C-l>", ":vert resize +5<CR>")
-
 -- Move lines easily
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
