f2c58a411e5d4819453e1018d70c932df8d71884
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-05-13 20:18:14 +0300

    remove nvim-sidebar

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -47,9 +47,6 @@ require("packer").startup(function(use)
   -- File tree
   use({ "kyazdani42/nvim-tree.lua" })
 
-  -- Sidebar
-  use({ "sidebar-nvim/sidebar.nvim" })
-
   -- Treesitter for better syntax highlighting
   use({
     "nvim-treesitter/nvim-treesitter",
@@ -192,7 +189,6 @@ require("user.theme")
 require("nvim-autopairs").setup({})
 
 require("user.file-tree")
-require("user.sidebar")
 require("user.treesitter")
 require("user.telescope")
 require("user.gitsigns")
--- neovim/.config/nvim/lua/user/sidebar.lua
+++
@@ -1,5 +0,0 @@
-require("sidebar-nvim").setup({
-  open = false,
-  side = "right",
-  sections = { "datetime", "git", "diagnostics", "todos", "containers", "symbols" },
-})
