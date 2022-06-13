d33bca2dba204c80ba58eaea579bb5da581ea177
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-06-13 20:22:16 +0300

    remove unnecessary echos

--- bin/chown-huawei-wmi.sh
+++ bin/chown-huawei-wmi.sh
@@ -2,9 +2,5 @@
 
 readonly WMI=/sys/devices/platform/huawei-wmi
 
-echo "started"
-
-echo "$(/bin/whoami)"
-
 chmod g+w "$WMI/leds/platform::micmute/brightness"
 chown root:huawei-wmi "$WMI/leds/platform::micmute/brightness"
