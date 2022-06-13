09bd6f4c7477e282f54f3b97d8451f572bc74bec
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-06-13 22:58:16 +0300

    set initial mic state on startup

---
+++ bin/initial-mic-state.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+pactl list sources | grep -qi "Mute: yes"
+state="$?"
+
+if [[ "$state" == "0" ]]; then
+	echo "0" >/sys/devices/platform/huawei-wmi/leds/platform::micmute/brightness
+else
+	echo "1" >/sys/devices/platform/huawei-wmi/leds/platform::micmute/brightness
+fi
