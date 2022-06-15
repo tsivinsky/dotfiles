f948c5ba54df87840b7243478113fd6790ce168c
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-06-15 22:42:15 +0300

    use nvim api for highlights

--- neovim/.config/nvim/lua/user/theme.lua
+++ neovim/.config/nvim/lua/user/theme.lua
@@ -2,8 +2,11 @@ vim.g.edge_diagnostic_virtual_text = "colored"
 vim.g.edge_better_performance = 1
 vim.g.tokyonight_style = "night"
 
+vim.api.nvim_set_hl(0, "SpellBad", {
+  fg = "red",
+})
+
 vim.cmd([[
 colorscheme gruvbox-material
-hi SpellBad guifg=red
 hi clear SignColumn
 ]])
