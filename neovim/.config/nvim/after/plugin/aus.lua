f5443d2e7971f405a1ed84c108d579d4ffd7cf09
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-06-19 23:39:23 +0300

    neovim: remove goimports autocmd

--- neovim/.config/nvim/after/plugin/aus.lua
+++ neovim/.config/nvim/after/plugin/aus.lua
@@ -32,11 +32,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  group = group,
-  callback = function()
-    vim.cmd(":silent :!goimports -w %")
-  end,
-})
