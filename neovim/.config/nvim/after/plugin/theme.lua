8dfcd07c03351747cef02b304e7805ddb45f8458
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-06-28 10:16:55 +0300

    neovim: remove rose-pine colorscheme

--- neovim/.config/nvim/after/plugin/theme.lua
+++ neovim/.config/nvim/after/plugin/theme.lua
@@ -1,12 +1,8 @@
-vim.g.edge_diagnostic_virtual_text = "colored"
-vim.g.edge_better_performance = 1
-vim.g.tokyonight_style = "night"
-
 vim.api.nvim_set_hl(0, "SpellBad", {
   fg = "red",
 })
 
 vim.cmd([[
-colorscheme rose-pine
+colorscheme gruvbox-material
 hi clear SignColumn
 ]])
