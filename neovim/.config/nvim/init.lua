714ac5d2b56976984c62221550c5529687398253
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-09-10 18:16:53 +0300

    neovim: remove `locals` section

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,10 +1,7 @@
--- Locals
-local fn = vim.fn
-
 -- Install packer automagically
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = vim.fn.system({
     "git",
     "clone",
     "--depth",
