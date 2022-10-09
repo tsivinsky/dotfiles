305533d8541b5772bc2e2b7ad5168a8df2576333
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-10-09 14:43:32 +0300

    sway: add `control-brightness.sh` script
    it allows to change external monitor's brightness

---
+++ bin/control-brightness.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+active_output=$(swaymsg -p -t get_outputs | grep '(focused)' | awk '{ printf $2 }')
+
+device=""
+if [[ "$active_output" == "HDMI-A-1" ]]; then
+	device="ddcci5"
+else
+	device="amdgpu_bl0"
+fi
+
+param=""
+if [[ "$1" == "up" ]]; then
+	param="+"
+else
+	param="-"
+fi
+
+brightnessctl -d "$device" set 5%$param
