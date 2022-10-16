03839e7f61a0b52800af5b8ae4cd4c072cd83af7
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-10-16 22:07:13 +0300

    neovim: update nvim-cmp

--- neovim/.config/nvim/after/plugin/lsp.lua
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -62,7 +62,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local lsps_with_disabled_formatting = { "tsserver", "gopls", "jsonls", "html", "sumneko_lua" }
