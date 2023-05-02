ce7312a2ebdf198b86b86dcd8f8add7040aa0c91
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-05-02 17:53:41 +0300

    neovim: remove `:ClearMessages` cmd

--- neovim/.config/nvim/after/plugin/cmds.lua
+++ neovim/.config/nvim/after/plugin/cmds.lua
@@ -15,10 +15,6 @@ vim.api.nvim_create_user_command("GitShowCommit", function()
   vim.cmd(":Git show " .. commit)
 end, {})
 
-vim.api.nvim_create_user_command("ClearMessages", function()
-  require("notify").dismiss()
-end, {})
-
 vim.api.nvim_create_user_command("OpenUnsavedBuffers", function()
   require("daniil.utils").open_unsaved_buffers()
 end, {})
