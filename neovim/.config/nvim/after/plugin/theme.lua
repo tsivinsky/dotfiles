e095082293832e2586a7a01f6626ef69f1bc3cbb
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-07-27 23:04:31 +0300

    neovim: change gruvbox colorscheme variant

--- neovim/.config/nvim/after/plugin/theme.lua
+++ neovim/.config/nvim/after/plugin/theme.lua
@@ -2,6 +2,12 @@ vim.api.nvim_set_hl(0, "SpellBad", {
   fg = "red",
 })
 
+require("gruvbox").setup({
+  italic = {
+    comments = true,
+  },
+})
+
 vim.cmd([[
 colorscheme gruvbox
 hi clear SignColumn
--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -19,7 +19,7 @@ require("packer").startup(function(use)
   use({ "folke/neodev.nvim" })
 
   -- Colorschemes go here
-  use({ "sainnhe/gruvbox-material" })
+  use({ "sainnhe/gruvbox-material", "ellisonleao/gruvbox.nvim" })
 
   -- Make commenting code great
   use({
