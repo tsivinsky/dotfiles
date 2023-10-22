0f432164ea985bbf1bfae76770ca748bacc29329
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-10-22 19:25:05 +0300

    neovim: add `Format` command

--- neovim/.config/nvim/after/plugin/commands.lua
+++ neovim/.config/nvim/after/plugin/commands.lua
@@ -8,3 +8,14 @@ vim.api.nvim_create_user_command("FormattingEnable", formatting.enable, {})
 vim.api.nvim_create_user_command("FormattingDisable", formatting.disable, {})
 vim.api.nvim_create_user_command("FormattingToggle", formatting.toggle, {})
 vim.api.nvim_create_user_command("FormattingStatus", formatting.status, {})
+
+vim.api.nvim_create_user_command("Format", function()
+  local bf = vim.bo.filetype
+
+  if bf == "typescript" or bf == "typescriptreact" then
+    vim.cmd(":!npx prettier --loglevel silent -w %")
+    return
+  end
+
+  vim.lsp.buf.format()
+end, {})
