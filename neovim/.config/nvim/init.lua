2b6bea20fa102f0e621e4929364ef34fb23572d9
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-10-31 22:12:19 +0300

    neovim: install `eyeliner` plugin

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -58,6 +58,16 @@ require("packer").startup(function(use)
 
   use({ "nvim-lualine/lualine.nvim" })
 
+  use({
+    "jinh0/eyeliner.nvim",
+    config = function()
+      require("eyeliner").setup({
+        highlight_on_key = true,
+        dim = true,
+      })
+    end,
+  })
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
