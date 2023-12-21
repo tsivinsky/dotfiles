0efa7e6b241f6a0cdd7bd5188a67f98ea09c2765
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-12-21 15:40:14 +0300

    neovim: add remap for live_grep

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -52,4 +52,5 @@ telescope.setup({
 telescope.load_extension("fzf")
 
 vim.keymap.set("n", "<leader>f", builtin.find_files)
+vim.keymap.set("n", "<leader>s", builtin.live_grep)
 vim.keymap.set("n", "<leader>p", vim.cmd.Telescope)
