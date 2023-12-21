6e08d7952362f3294e5b0983e0c1f7505a05a594
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-12-21 15:38:19 +0300

    neovim: focus undotree window when opening

--- neovim/.config/nvim/after/plugin/undotree.lua
+++ neovim/.config/nvim/after/plugin/undotree.lua
@@ -1,4 +1,7 @@
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>u", function()
+  vim.cmd.UndotreeToggle()
+  vim.cmd.UndotreeFocus()
+end)
