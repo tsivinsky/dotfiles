89229e915fcad9807c5a1425292c586cd98f801a
Daniil Tsivinsky <daniil@tsivinsky.com>
2024-03-08 23:50:16 +0300

    Change kitty to wezterm

---
+++ config/.wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "ayu"
+
+config.enable_tab_bar = false
+config.enable_wayland = true
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.font = wezterm.font("JetBrains Mono", {
+  weight = "Medium",
+})
+
+return config
