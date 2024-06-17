0aafb90532809d1342b696384f031a3736ed6d60
Daniil Tsivinsky <daniil@tsivinsky.com>
2024-06-17 23:06:35 +0300

    neovim: update autotag configuration

--- neovim/.config/nvim/after/plugin/treesitter.lua
+++ neovim/.config/nvim/after/plugin/treesitter.lua
@@ -28,7 +28,12 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
   },
-  autotag = {
-    enable = true,
+})
+
+require("nvim-ts-autotag").setup({
+  opts = {
+    enable_close = true,
+    enable_rename = true,
+    enable_close_on_slash = false,
   },
 })
