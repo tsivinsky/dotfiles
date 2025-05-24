deafcf9a9841c9d3ecbda62d9a39292bdef17a89
Daniil Tsivinsky <daniil@tsivinsky.com>
2025-05-24 18:22:24 +0300

    neovim: update tag comment colors

--- neovim/.config/nvim/after/plugin/treesitter.lua
+++ neovim/.config/nvim/after/plugin/treesitter.lua
@@ -37,3 +37,10 @@ require("nvim-ts-autotag").setup({
     enable_close_on_slash = false,
   },
 })
+
+-- colors generated using coolors.co
+-- https://coolors.co/008bf8-43aa8b-ffc145-db4b4b
+vim.api.nvim_set_hl(0, "@comment.todo", { bg = "none", fg = "#008BF8", italic = true, blend = 100 })
+vim.api.nvim_set_hl(0, "@comment.note", { bg = "none", fg = "#43AA8B", italic = true, blend = 100 })
+vim.api.nvim_set_hl(0, "@comment.warning", { bg = "none", fg = "#FFC145", italic = true, blend = 100 })
+vim.api.nvim_set_hl(0, "@comment.error", { bg = "#db4b4b", fg = "white", italic = true, blend = 100 })
